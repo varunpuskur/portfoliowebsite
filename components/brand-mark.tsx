cc0e6a0e30a1b33c2c65d9bc18eb81ee354ba4e5
Varun <puskurvarun@gmail.com>
2026-09-17 20:04:15 -0400

    2026: fix mobile logo and crowded typography

--- components/brand-mark.tsx
+++ components/brand-mark.tsx
@@ -1,5 +1,14 @@
+import { useId } from 'react';
+
 export function BrandMark() {
+  const maskId = useId();
+
   return <svg className="brand-mark" viewBox="11 181 1481 900" aria-hidden="true" focusable="false">
-    <image href="/images/varun-logo.png" width="1500" height="1125" className="brand-original" />
+    <defs>
+      <mask id={maskId} maskUnits="userSpaceOnUse" x="0" y="0" width="1500" height="1125">
+        <image href="/images/varun-logo.png" width="1500" height="1125" />
+      </mask>
+    </defs>
+    <rect x="11" y="181" width="1481" height="900" fill="#1e242a" mask={`url(#${maskId})`} />
   </svg>;
 }
